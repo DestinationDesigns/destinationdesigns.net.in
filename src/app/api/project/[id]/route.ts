434e10f565ed1d5c33b61a5379b21b8b9936bd3a
NevilleJS <69287021+s3dman@users.noreply.github.com>
2024-01-15 16:07:54 +0530

    setting up quer

---
+++ src/app/api/project/[id]/route.ts
@@ -0,0 +1,19 @@
+import { ObjectId } from "mongodb";
+import connectMongoDB from "../../../../lib/mongodb";
+import { NextResponse } from "next/server";
+
+// PROJECT
+export async function GET(req, { params }) {
+	try {
+		const { id } = params;
+		const client = await connectMongoDB();
+		const db = client.db("DestinationDesigns");
+		const project = await db
+			.collection("Project")
+			.findOne({ _id: new ObjectId(id) });
+		return NextResponse.json(project);
+	} catch (err) {
+		console.log("Error in GET:", err);
+		return NextResponse.json({});
+	}
+}
--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,14 +1,31 @@
 import connectMongoDB from "../../../lib/mongodb";
 import { NextResponse } from "next/server";
 
+// PROJECTS
+
 export async function GET() {
-  try {
-    const client = await connectMongoDB();
-    const db = client.db("DestinationDesigns");
-    const projects = await db.collection("Projects").find({}).toArray();
-    console.log(projects);
-    return NextResponse.json({ projects });
-  } catch (err) {
-    console.log("Error in GET:", err);
-  }
+	try {
+		const client = await connectMongoDB();
+		const db = client.db("DestinationDesigns");
+		const projects = await db
+			.collection("Project")
+			.find({})
+			.sort({
+				class: 1,
+				vindex: 1,
+				_id: 1,
+			})
+			.project({
+				name: 1,
+				class: 1,
+				group: 1,
+				featured: 1,
+				vindex: 1,
+			})
+			.toArray();
+		return NextResponse.json(projects);
+	} catch (err) {
+		console.log("Error in GET:", err);
+		return NextResponse.json({});
+	}
 }
