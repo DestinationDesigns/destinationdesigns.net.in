5ef383a4215ce24769a31877173eeb72fd94e6e9
NevilleJS <69287021+s3dman@users.noreply.github.com>
2024-02-14 10:50:58 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import Footer from "@/components/Footer";
 
 export default function RootLayout({
@@ -15,6 +16,7 @@ export default function RootLayout({
 			<body>
 				<div className="container">{children}</div>
 				<Footer />
+				<Analytics />
 			</body>
 		</html>
 	);
